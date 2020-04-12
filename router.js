fc6495a9d4ddf41c1858917030c2e4295d37e210
Erik Flores <erik.flores@colorado.edu>
2020-04-12 15:35:41 -0600

    updates

--- router.js
+++ router.js
@@ -1,4 +1,5 @@
 import Stories from './pages/stories.js'
+import Item from './pages/item.js'
 
 const router = new Navigo(null, true, '#')
 
@@ -12,11 +13,12 @@ export default class RouterHandler {
             { path : '/', page: Stories },
             { path : '/new', page: Stories },
             { path : '/ask', page: Stories },
-            { path : '/show', page: Stories }
+            { path : '/show', page: Stories },
+            { path : '/item', page: Item }
         ]
 
         routes.forEach(({ path, page }) => {
-            router.on(path,  () => {
+            router.on(path, () => {
                 page(path)
             }).resolve()
         })
